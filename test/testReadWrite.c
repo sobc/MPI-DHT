5f7a269169ed826707f2846383e745438680184d
Max Lübke <max.luebke@uni-potsdam.de>
2023-11-10 10:43:18 +0100

    rework ReadWrite tests

--- test/testReadWrite.c
+++ test/testReadWrite.c
@@ -1,7 +1,8 @@
-#include <DHT/DHT.h>
+#include <DHT_ucx/DHT.h>
 #include <mpi.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ucp/api/ucp_compat.h>
 #include <ucs/type/status.h>
 #include <xxhash.h>
@@ -18,49 +19,56 @@ uint64_t xxhashWrapper(int len, const void *key) {
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
+  uint32_t key;
+  uint32_t data;
   int rank;
+  int comm_size;
+  int status;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
-  DHT *object = DHT_create(MPI_COMM_WORLD, 50, 4, 4, xxhashWrapper);
+  DHT *object = DHT_create(MPI_COMM_WORLD, 50, sizeof(uint32_t),
+                           sizeof(uint32_t), xxhashWrapper);
 
-  fprintf(stdout, "Rank %d: Initialization done!\n", rank);
-  fflush(stdout);
+  if (object == NULL) {
+    fprintf(stderr, "Error while creating DHT. Aborting ...\n");
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
 
-  sleep(5);
+  key = rank;
+  data = rank;
 
-  if (rank == 0) {
-    uint32_t key = 123;
-    uint32_t data = 456;
+  status = DHT_write(object, &key, &data, NULL, NULL);
 
-    if (DHT_SUCCESS != DHT_write(object, &key, &data, NULL, NULL)) {
-      fprintf(stderr, "Calling write\n");
-      fflush(stderr);
-    }
+  if (DHT_SUCCESS != status) {
+    fprintf(stderr, "Error during write. Aborting ...\n");
+    MPI_Abort(MPI_COMM_WORLD, status);
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
-  sleep(2);
 
-  if (rank == 1) {
+  key = (rank + 1) % comm_size;
 
-    uint32_t key = 123;
-    uint32_t data;
+  status = DHT_read(object, &key, &data);
 
-    int status = DHT_read(object, &key, &data);
-
-    if (DHT_MPI_ERROR == status) {
-      fprintf(stderr, "ret = %d\n", status);
-      return 1;
-    }
-
-    printf("Read data is: %d\n", data);
+  if (DHT_MPI_ERROR == status) {
+    fprintf(stderr, "Error during read. Aborting ...\n");
+    MPI_Abort(MPI_COMM_WORLD, status);
+    return 1;
   }
 
+  if (key != data) {
+    fprintf(stdout, "Rank %d: FAILURE!\n", rank);
+  } else {
+    fprintf(stdout, "Rank %d: OK!\n", rank);
+  }
 
-  sleep(2);
-
-  DHT_free(object, NULL, NULL);
+  status = DHT_free(object, NULL, NULL);
+  if (DHT_SUCCESS != status) {
+    fprintf(stderr, "Error during free. Aborting ...\n");
+    MPI_Abort(MPI_COMM_WORLD, status);
+  }
 
   MPI_Finalize();
 }
